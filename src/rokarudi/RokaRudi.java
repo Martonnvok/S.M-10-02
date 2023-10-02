305174a8a0e0db956d16c10d86a13343dc289c60
Martonnvok <martonsom@gmail.com>
2023-10-02 13:27:54 +0200

    KartyaTrukk feladat újra csinálása

--- src/rokarudi/RokaRudi.java
+++ src/rokarudi/RokaRudi.java
@@ -1,20 +1,61 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package rokarudi;
 
-/**
- *
- * @author somogyvari.m.peter
- */
+import java.util.Scanner;
+
 public class RokaRudi {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        String lapok[] = new String[21];
+        lapok = feltolt();
+        int vLap = 0;
+        for (int i = 0; i < 3; i++) {
+            lapok(lapok);
+            beolvasas(vLap);
+            kever();
+        }
+
+        kiir();
     }
-    
+
+    private static String[] feltolt() {
+        String lap[] = new String[21];
+        String jelek[] = {"Piros", "Tök", "Zöld", "Makk"};
+        String rSzamok[] = {"IX", "X", "VIII", "Felső", "Alsó"};
+        int index = 0;
+        for (int i = 0; i < jelek.length; i++) {
+            for (int j = 0; j < rSzamok.length; j++) {
+                lap[index++] = jelek[i] + "_" + rSzamok[j];
+            }
+
+        }
+        return lap;
+    }
+
+    private static void lapok(String[] lap) {
+        for (int i = 0; i < lap.length; i++) {
+            if (i % 3 == 0) {
+                System.out.println("");
+            }
+            System.out.print(lap[i] + ", ");
+        }
+    }
+
+    private static void beolvasas(int vLap) {
+        do {
+            System.out.print("Kérek egy oszlopot (1-3): ");
+            vLap = sc.nextInt();
+        } while (vLap >= 3 && vLap <= 1);
+       
+    }
+
+    private static void kever() {
+
+    }
+
+    private static void kiir() {
+
+    }
+
 }
