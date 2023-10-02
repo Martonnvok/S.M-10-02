94baf7d1dab43123fe6200a177870110f5c0866d
Martonnvok <martonsom@gmail.com>
2023-10-02 13:39:00 +0200

    KartyaTrukk feladat minta felépítése

--- src/rokarudi/RokaRudi.java
+++ src/rokarudi/RokaRudi.java
@@ -13,7 +13,7 @@ public class RokaRudi {
         for (int i = 0; i < 3; i++) {
             lapok(lapok);
             beolvasas(vLap);
-            kever(vLap);
+            kever(vLap, lapok);
         }
 
         kiir(lapok);
@@ -21,7 +21,7 @@ public class RokaRudi {
 
     private static String[] feltolt() {
         String lap[] = new String[21];
-        String jelek[] = {"Piros", "Tök", "Zöld", "Makk"};
+        String jelek[] = {"P", "T", "Z", "M"};
         String rSzamok[] = {"IX", "X", "VIII", "Felső", "Alsó"};
         int index = 0;
         for (int i = 0; i < jelek.length; i++) {
@@ -39,10 +39,12 @@ public class RokaRudi {
                 System.out.println("");
             }
             System.out.print(lap[i] + ", ");
+
         }
     }
 
     private static void beolvasas(int vLap) {
+        System.out.println("");
         do {
             System.out.print("Kérek egy oszlopot (1-3): ");
             vLap = sc.nextInt();
@@ -50,20 +52,28 @@ public class RokaRudi {
 
     }
 
-    private static void kever(int vLap) {
+    private static void kever(int vLap, String lapok[]) {
 
         switch (vLap) {
             case 1:
-                for (int i = 0; i < 10; i++) {
-
+                for (int i = 0; i < lapok.length; i++) {
+                    lapok[i - 7] = lapok[20 - (i - 1) * 3];
+                    lapok[i - 14] = lapok[19 - (i - 1) * 3];
+                    lapok[i - 21] = lapok[21 - (i - 1) * 3];
                 }
+
             case 2:
-                for (int i = 0; i < 10; i++) {
-
+                for (int i = 0; i < lapok.length; i++) {
+                    lapok[i - 7] = lapok[19 - (i - 1) * 3];
+                    lapok[i - 14] = lapok[20 - (i - 1) * 3];
+                    lapok[i - 21] = lapok[21 - (i - 1) * 3];
                 }
-            case 3:
-                for (int i = 0; i < 10; i++) {
 
+            case 3:
+                for (int i = 0; i < lapok.length; i++) {
+                    lapok[i - 7] = lapok[19 - (i - 1) * 3];
+                    lapok[i - 14] = lapok[21 - (i - 1) * 3];
+                    lapok[i - 21] = lapok[20 - (i - 1) * 3];
                 }
 
         }
@@ -71,7 +81,7 @@ public class RokaRudi {
     }
 
     private static void kiir(String tomb[]) {
-        System.out.println("Választott lap: "+ tomb[11]);
+        System.out.println("Választott lap: " + tomb[11]);
     }
 
 }
